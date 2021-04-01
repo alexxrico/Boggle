fe73c1771d8ca6247b4c152f22c91543980e92a8
alexxrico <alexxricoart@gmail.com>
2021-04-01 14:31:48 -0700

    Imagen

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,6 +1,10 @@
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 public class Boggle extends JFrame{
     private static final long serialVersionUID = 1L;
     public Boggle(){
@@ -12,10 +16,18 @@ public class Boggle extends JFrame{
     }
     private void IniciarComponentes(){
         JPanel panel = new JPanel(); // Creacion del panel
+        panel.setLayout(null);
         this.getContentPane().add(panel);
         
-        JLabel etiqueta = new JLabel("Juego Boggle!!");
+        JLabel etiqueta = new JLabel("JUEGO BOGGLE!!");
         panel.add(etiqueta);
+        etiqueta.setBounds(85, 5, 300, 80);
+        etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+        etiqueta.setFont(new Font ("arial",Font.BOLD,20)); // Estilo de la letra
+
+        JLabel imagen = new JLabel (new ImageIcon("uach.png"));
+        imagen.setBounds(40, 80, 400, 400);
+        panel.add(imagen);
     }
     public static void main(String[] args) {
         Boggle Ventana = new Boggle();
