6655eae6dafb91abf3a5296a8d16591a391f8e23
alexxrico <alexxricoart@gmail.com>
2021-03-31 19:40:07 -0700

    Etiqueta

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,14 +1,24 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class Boggle extends JFrame{
     private static final long serialVersionUID = 1L;
     public Boggle(){
         setSize(500,500); // Tamaño de la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Terminar ejecucion al cerrar ventana
-        setTitle("Boggle");
+        setTitle("BOGGLE");
         setLocationRelativeTo(null); // Pantalla en el centro
+        IniciarComponentes();
+    }
+    private void IniciarComponentes(){
+        JPanel panel = new JPanel(); // Creacion del panel
+        this.getContentPane().add(panel);
+        
+        JLabel etiqueta = new JLabel("Juego Boggle!!");
+        panel.add(etiqueta);
     }
     public static void main(String[] args) {
-        Boggle p1 = new Boggle();
-        p1.setVisible(true);
+        Boggle Ventana = new Boggle();
+        Ventana.setVisible(true);
     }
 }
