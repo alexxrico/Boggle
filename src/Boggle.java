cef55993e0f9deaffad230f75fe4510c351a47af
alexxrico <alexxricoart@gmail.com>
2021-04-01 20:50:56 -0700

    Diseño de portada

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,49 +1,89 @@
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 public class Boggle extends JFrame{
-    public JPanel panel;
     private static final long serialVersionUID = 1L;
+    
+    // Variables de portada
+    public JPanel panel;
+
+    public static void main(String[] args) {
+        Boggle Ventana = new Boggle();
+        Ventana.setVisible(true);
+    }
     public Boggle(){
         setSize(800,500); // Tamaño de la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Terminar ejecucion al cerrar ventana
         setTitle("BOGGLE");
         setLocationRelativeTo(null); // Pantalla en el centro
-        IniciarComponentes();
+        Portada();
         
     }
-    private void IniciarComponentes(){
-        Paneles();
-        Etiquetas();
-        Icons();
-        
+    // Diseño de la ventda de la portada
+    private void Portada(){
+        PanelesP();
+        EtiquetasP();
+        IconsP();
+        BotonesP();
     }
-    private void Paneles(){
-         panel = new JPanel(); // Creacion del panel
+    private void PanelesP(){
+        panel = new JPanel(); // Creacion del panel
         panel.setLayout(null);
         this.getContentPane().add(panel);
     }
-    private void Etiquetas(){
+    private void EtiquetasP(){
         JLabel etiqueta = new JLabel("JUEGO BOGGLE!!");
-        panel.add(etiqueta);
         etiqueta.setBounds(250, 5, 300, 40);
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
         etiqueta.setFont(new Font ("arial",Font.BOLD,20)); // Estilo de la letra
+        panel.add(etiqueta);
+
+        JLabel descrip = new JLabel("Bienvenido al juego Boggle!!");
+        descrip.setBounds(430, 100, 300, 40);
+        descrip.setHorizontalAlignment(SwingConstants.CENTER);
+        descrip.setFont(new Font ("arial",Font.BOLD,18));
+        panel.add(descrip);
+
+        JLabel descrip1 = new JLabel("¿Lograras susperar el reto Jhon Cena?");
+        descrip1.setBounds(400, 120, 350, 40);
+        descrip1.setHorizontalAlignment(SwingConstants.CENTER);
+        descrip1.setFont(new Font ("arial",Font.BOLD,18));
+        panel.add(descrip1);
+
+        JLabel jess = new JLabel("Jessica Yamile Salcido Cadena - 329575");
+        jess.setBounds(400, 180, 350, 40);
+        jess.setHorizontalAlignment(SwingConstants.CENTER);
+        jess.setFont(new Font ("arial",Font.BOLD,18));
+        panel.add(jess);
+
+        JLabel alex = new JLabel("David Alejandro Perez Rico - 320753");
+        alex.setBounds(400, 200, 350, 40);
+        alex.setHorizontalAlignment(SwingConstants.CENTER);
+        alex.setFont(new Font ("arial",Font.BOLD,18));
+        panel.add(alex);
     }
-    private void Icons(){
+    private void IconsP(){
         ImageIcon imagen1 = new ImageIcon("uach.png");
         JLabel imagen = new JLabel ();
         imagen.setBounds(40, 50, 320, 400);
         imagen.setIcon(new ImageIcon(imagen1.getImage().getScaledInstance(imagen.getWidth(), imagen.getHeight(), Image.SCALE_SMOOTH))); // Reescalado de imagen
         panel.add(imagen);
     }
-    public static void main(String[] args) {
-        Boggle Ventana = new Boggle();
-        Ventana.setVisible(true);
+    private void BotonesP(){
+        JButton boton1 = new JButton();
+        boton1.setText("START");
+        boton1.setEnabled(true); // Boton habilitado
+        boton1.setBounds(520, 320, 100, 40);
+        boton1.setForeground(Color.GREEN);
+        boton1.setFont(new Font("arial",Font.BOLD,18));
+        panel.add(boton1);
     }
+    
 }
