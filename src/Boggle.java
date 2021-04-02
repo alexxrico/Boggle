bd27c9ddf361872227c6e6a9833f95c8b9ef54f1
alexxrico <alexxricoart@gmail.com>
2021-04-01 19:14:38 -0700

    Reordenamiento

--- src/Boggle.java
+++ src/Boggle.java
@@ -7,28 +7,38 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 public class Boggle extends JFrame{
+    public JPanel panel;
     private static final long serialVersionUID = 1L;
     public Boggle(){
-        setSize(500,500); // Tamaño de la ventana
+        setSize(800,500); // Tamaño de la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Terminar ejecucion al cerrar ventana
         setTitle("BOGGLE");
         setLocationRelativeTo(null); // Pantalla en el centro
         IniciarComponentes();
+        
     }
     private void IniciarComponentes(){
-        JPanel panel = new JPanel(); // Creacion del panel
+        Paneles();
+        Etiquetas();
+        Icons();
+        
+    }
+    private void Paneles(){
+         panel = new JPanel(); // Creacion del panel
         panel.setLayout(null);
         this.getContentPane().add(panel);
-        
+    }
+    private void Etiquetas(){
         JLabel etiqueta = new JLabel("JUEGO BOGGLE!!");
         panel.add(etiqueta);
-        etiqueta.setBounds(85, 5, 300, 80);
+        etiqueta.setBounds(250, 5, 300, 40);
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
         etiqueta.setFont(new Font ("arial",Font.BOLD,20)); // Estilo de la letra
-
+    }
+    private void Icons(){
         ImageIcon imagen1 = new ImageIcon("uach.png");
         JLabel imagen = new JLabel ();
-        imagen.setBounds(40, 70, 320, 400);
+        imagen.setBounds(40, 50, 320, 400);
         imagen.setIcon(new ImageIcon(imagen1.getImage().getScaledInstance(imagen.getWidth(), imagen.getHeight(), Image.SCALE_SMOOTH))); // Reescalado de imagen
         panel.add(imagen);
     }
