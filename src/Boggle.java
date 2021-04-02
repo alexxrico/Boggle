cb040c6da6f31c096144724bc3c45f6ffe338f58
alexxrico <alexxricoart@gmail.com>
2021-04-02 15:08:42 -0700

    Acomodo

--- src/Boggle.java
+++ src/Boggle.java
@@ -11,12 +11,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
-public class Boggle extends JFrame{
+public class Boggle extends JFrame implements ActionListener{
     private static final long serialVersionUID = 1L;
     
     // Variables de portada
-    public JPanel panel;
+    public JPanel portada;
+    private JLabel etiqueta, descrip, descrip1, jess, alex, imagen, gif1;
     private JButton boton1;
+    private JRadioButton politica;
+    // Variables del inicio
+    public JPanel inicio;
     public static void main(String[] args) {
         Boggle Ventana = new Boggle();
         Ventana.setVisible(true);
@@ -27,10 +31,10 @@ public class Boggle extends JFrame{
         setTitle("BOGGLE");
         setLocationRelativeTo(null); // Pantalla en el centro
         Portada();
-        
     }
     // Diseño de la ventda de la portada
     private void Portada(){
+        
         PanelesP();
         EtiquetasP();
         IconsP();
@@ -39,79 +43,104 @@ public class Boggle extends JFrame{
         Gif();
     }
     private void PanelesP(){
-        panel = new JPanel(); // Creacion del panel
-        panel.setLayout(null);
-        this.getContentPane().add(panel);
+        portada = new JPanel(); // Creacion del portada
+        portada.setLayout(null);
+        this.getContentPane().add(portada);
     }
     private void EtiquetasP(){
-        JLabel etiqueta = new JLabel("JUEGO BOGGLE!!");
+        etiqueta = new JLabel("JUEGO BOGGLE!!");
         etiqueta.setBounds(250, 5, 300, 40);
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
         etiqueta.setFont(new Font ("arial",Font.BOLD,20)); // Estilo de la letra
-        panel.add(etiqueta);
+        portada.add(etiqueta);
 
-        JLabel descrip = new JLabel("Bienvenido al juego Boggle!!");
+        descrip = new JLabel("Bienvenido al juego Boggle!!");
         descrip.setBounds(430, 50, 300, 40);
-        descrip.setHorizontalAlignment(SwingConstants.CENTER);
+        descrip.setHorizontalAlignment(SwingConstants.LEFT);
         descrip.setFont(new Font ("arial",Font.BOLD,18));
-        panel.add(descrip);
+        portada.add(descrip);
 
-        JLabel descrip1 = new JLabel("¿Lograras susperar el reto John Cena?");
-        descrip1.setBounds(400, 80, 350, 40);
-        descrip1.setHorizontalAlignment(SwingConstants.CENTER);
+        descrip1 = new JLabel("¿Lograras susperar el reto John Cena?");
+        descrip1.setBounds(380, 80, 350, 40);
+        descrip1.setHorizontalAlignment(SwingConstants.LEFT);
         descrip1.setFont(new Font ("arial",Font.BOLD,18));
-        panel.add(descrip1);
+        portada.add(descrip1);
 
-        JLabel jess = new JLabel("Jessica Yamile Salcido Cadena - 329575");
-        jess.setBounds(400, 260, 350, 40);
-        jess.setHorizontalAlignment(SwingConstants.CENTER);
+        jess = new JLabel("Jessica Yamile Salcido Cadena - 329575");
+        jess.setBounds(30, 390, 350, 40);
+        jess.setHorizontalAlignment(SwingConstants.LEFT);
         jess.setFont(new Font ("arial",Font.BOLD,16));
-        panel.add(jess);
+        portada.add(jess);
 
-        JLabel alex = new JLabel("David Alejandro Perez Rico - 320753");
-        alex.setBounds(400, 280, 350, 40);
-        alex.setHorizontalAlignment(SwingConstants.CENTER);
+        alex = new JLabel("David Alejandro Perez Rico - 320753");
+        alex.setBounds(30, 420, 350, 40);
+        alex.setHorizontalAlignment(SwingConstants.LEFT);
         alex.setFont(new Font ("arial",Font.BOLD,16));
-        panel.add(alex);
+        portada.add(alex);
     }
     private void IconsP(){
         ImageIcon imagen1 = new ImageIcon("archivos\\uach.png");
-        JLabel imagen = new JLabel();
-        imagen.setBounds(40, 50, 320, 400);
+        imagen = new JLabel();
+        imagen.setBounds(70, 50, 250, 340);
         imagen.setIcon(new ImageIcon(imagen1.getImage().getScaledInstance(imagen.getWidth(), imagen.getHeight(), Image.SCALE_SMOOTH))); // Reescalado de imagen
-        panel.add(imagen);
+        portada.add(imagen);
     }
     private void BotonesP(){
         boton1 = new JButton();
         boton1.setText("START");
         boton1.setEnabled(false); // Boton habilitado
-        boton1.setBounds(540, 400, 100, 40);
+        boton1.setBounds(500, 400, 100, 40);
         boton1.setForeground(Color.GREEN);
         boton1.setFont(new Font("arial",Font.BOLD,18));
-        panel.add(boton1);
+        portada.add(boton1);
+        
+        boton1.addActionListener(this);
     }
     private void RadioBoton(){
-        JRadioButton politica = new JRadioButton("Acepto las consecuencias del reto JOHN CENA!");
+        politica = new JRadioButton("Acepto las consecuencias del reto JOHN CENA!");
         politica.setBounds(400, 360, 400, 20);
-        panel.add(politica);
-        // Accion del boton y vadilacion si esta seleccionado
-        ActionListener accion = new ActionListener(){ 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Habilitar o deshabiliatr el boton "Start"
-                if(politica.isSelected()==true){
-                    boton1.setEnabled(true); 
-                }else{
-                    boton1.setEnabled(false);
-                }
-            }
-        };
-        politica.addActionListener(accion);
+        portada.add(politica);
+        
+        politica.addActionListener(this);
     }
     private void Gif(){
         ImageIcon gif = new ImageIcon("archivos\\john1.gif");
-        JLabel gif1 = new JLabel(gif);
-        gif1.setBounds(400, 120, 350, 140);
-        panel.add(gif1);
+        gif1 = new JLabel(gif);
+        gif1.setBounds(350, 120, 400, 200);
+        portada.add(gif1);
+    }
+    private void VaciarVentana(){
+        etiqueta.setVisible(false);
+        descrip.setVisible(false);
+        descrip1.setVisible(false);
+        jess.setVisible(false);
+        alex.setVisible(false);
+        imagen.setVisible(false);
+        gif1.setVisible(false);
+        politica.setVisible(false);
+        boton1.setVisible(false);
+
+        portada.setVisible(false);
+    }
+    private void Inicio(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object evento = e.getSource();
+        // Evento si se selecciona el boton de aceptar concecuencias
+        if(evento == politica){
+            if(politica.isSelected()==true){
+                boton1.setEnabled(true); 
+            }else{
+                boton1.setEnabled(false);
+            }
+        }
+        // Evento si se selecciona el boton de "Satrt"
+        else if(evento == boton1){
+            VaciarVentana();
+            Inicio();
+        }
     }
 }
